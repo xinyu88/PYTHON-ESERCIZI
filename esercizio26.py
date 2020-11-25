fde8eb7b47868cf6e20da73ba85fce161aca1cbb
xinyu88 <xinyu1304@gmail.com>
2020-11-25 19:49:54 +0100

    esercizio26

---
+++ esercizio26.py
@@ -0,0 +1,20 @@
+#veicoli transitati
+
+contatore = 0
+
+print("inserire -1  per  termina ")
+s = []
+
+while True : #ciclo while termina quando l'utente inserisce -1
+    stipendio = int(input("inserisci i stipendi dei dipendenti"))
+    if stipendio == -1:
+        break  
+
+    else: 
+        contatore += 1
+        s.append(stipendio)
+        somma = sum(s)
+        
+        
+media = somma / contatore 
+print ("la media degli stipendi dei dipendenti è", media)  
